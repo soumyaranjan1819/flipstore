2f648c3ad6a57197cc71e7c5bb51a8b02cf6ef6a
[Soumya Ranjan] <soumyaranjan1819@gmail.com>
2022-12-15 17:04:18 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Home from './Pages/home'
 import ItemStore from './Pages/items'
 import Cart from './Pages/cart'
 import WishList from './Pages/wishlist'
+import Footer from './Components/Footer/index'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/shop' element={<ItemStore/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/wishlist' element={<WishList/>} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/shop' element={<ItemStore/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/wishlist' element={<WishList/>} />
+        </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
